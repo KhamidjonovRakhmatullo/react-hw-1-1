64719fdf93c0770efb1095473bd54d61ce13818d
KHAMIDJONOV RAKHMATULLO <khamidjonovrakhmatullo@KHAMIDJONOVs-MacBook-Air.local>
2024-03-19 14:53:25 +0900

    comment

---
+++ src/navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+
+const NavbarComponent = () => {
+  return (
+    <div style={{
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "20px 30px",
+        backgroundColor: "GrayText"
+    }}> 
+    <Link to="caravan">
+        <p style={{cursor: "pointer", color: "white"}}>Caravan</p>
+    </Link>
+    <Link to="tuning">
+        <p style={{cursor: "pointer", color: "white"}}>Tuning</p>
+    </Link>
+    <Link to="motor">
+        <p style={{cursor: "pointer", color: "white"}}>Motor</p>
+    </Link>
+    <Link to="usedcars">
+        <p style={{cursor: "pointer", color: "white"}}>Used-Cars</p>
+    </Link>
+    <Link to="camping">
+        <p style={{cursor: "pointer", color: "white"}}>Camping</p>
+    </Link>
+    </div>
+  )
+}
+
+export default NavbarComponent
---
+++ src/router.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CaravanComponent from './caravan';
+import MotorComponent from './motor';
+import TuningComponent from './tuning';
+import UsedcarsComponent from './usedcars';
+import CampingComponent from './camping';
+import NavbarComponent from './navbar';
+
+const RouterComponent = () => {
+  return (
+    <BrowserRouter>
+    <NavbarComponent/>
+    <Routes>
+        <Route path='caravan' element={<CaravanComponent/>}/>
+        <Route path='motor' element={<MotorComponent/>}/>
+        <Route path='tuning' element={<TuningComponent/>}/>
+        <Route path='usedcars' element={<UsedcarsComponent/>}/>
+        <Route path='camping' element={<CampingComponent/>}/>
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouterComponent
